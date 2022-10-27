203b748a0260d84045fcaca265725cc36665573b
linbaoxue <12471729@qq.com>
2022-10-27 19:57:45 +0800

    first commit

---
+++ src/main/java/cn/kule/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.kule.git;
+
+/**
+ * author kule
+ * date 2022/10/27
+ * project git-test
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
